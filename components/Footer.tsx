c51a4e6470202187f1e69b699065800302287b00
CLawDann <daniel16151@gmail.com>
2024-02-14 13:31:17 -0800

    Adjusted Footer.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
-import styles from './Footer.module.css'
 
 const Footer: React.FC = () => {
   return (
-    <footer className={styles.footerBig}>
+    <footer className="backdrop-blur-md flex justify-between gap-2 z-20 w-full md:px-4 bg-white/10 max-w-screen-2xl mx-auto p-2">
       <p>© 2024 Joe Lee. All rights reserved.</p>
       <Link href="/license">
         License
