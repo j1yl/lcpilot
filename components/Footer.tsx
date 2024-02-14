91bea26886c139e7fbc621e203d7292c371cb3ef
CLawDann <daniel16151@gmail.com>
2024-02-14 13:20:41 -0800

    Updated Footer and License even further with Button and adjusted font.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from 'react'
+import Link from 'next/link'
+import styles from './Footer.module.css'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="footer">
-      {}
+    <footer className={styles.footerBig}>
       <p>© 2024 Joe Lee. All rights reserved.</p>
+      <Link href="/license">
+        License
+      </Link>
     </footer>
   );
 };
 
 export default Footer;
-
