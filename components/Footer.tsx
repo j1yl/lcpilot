8b45d04820b0425e299374b4ecf122baa18dc5fa
CLawDann <daniel16151@gmail.com>
2024-02-14 12:44:37 -0800

    Updated Footer and License

---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="footer">
+      {}
+      <p>© 2024 Joe Lee. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
+
