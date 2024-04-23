ce2a43fb78a2a553ca4874c7301f144407b6b7f2
CLawDann <daniel16151@gmail.com>
2024-04-22 22:16:46 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
-import Image from 'next/image';
+import SessionWrapper from '@/components/SessionWrapper';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,19 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} flex min-h-screen flex-col bg-neutral-950 text-xs text-white`}
-      >
-        <Image
-          src={'/background.jpg'}
-          alt={'cool laptop'}
-          fill
-          className="absolute left-0 top-0 z-0 aspect-video w-full object-center opacity-20"
-        />
-        <Navbar />
-        <div className="z-20 mx-auto flex h-full w-full max-w-screen-2xl flex-grow">{children}</div>
-      </body>
-    </html>
+    <SessionWrapper>
+      <html lang="en">
+        <body
+          className={`${inter.className} flex min-h-screen flex-col bg-neutral-950 text-xs text-white`}
+        >
+          <Navbar />
+          <div className="z-20 mx-auto flex h-full w-full flex-grow">{children}</div>
+        </body>
+      </html>
+    </SessionWrapper>
   );
 }
