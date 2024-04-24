50ed51409cb9d96b8992adac2e8fb37f4df92716
CLawDann <daniel16151@gmail.com>
2024-04-24 11:39:49 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+//import SessionWrapper from '@/components/SessionWrapper';
 import Image from 'next/image';
 import Footer from '@/components/Footer';
 
@@ -18,20 +19,22 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className} flex min-h-screen flex-col bg-neutral-950 text-xs text-white`}
-      >
-        <Image
-          src={'/background.jpg'}
-          alt={'cool laptop'}
-          fill
-          className="absolute left-0 top-0 z-0 aspect-video w-full object-center opacity-20"
-        />
-        <Navbar />
-        <div className="z-20 mx-auto flex h-full w-full max-w-screen-2xl flex-grow">{children}</div>
-        <Footer />
-      </body>
-    </html>
+    //<SessionWrapper>
+      <html lang="en">
+        <body
+          className={`${inter.className} flex min-h-screen flex-col bg-neutral-950 text-xs text-white`}
+        >
+          <Image
+            src={'/background.jpg'}
+            alt={'cool laptop'}
+            fill
+            className="absolute left-0 top-0 z-0 aspect-video w-full object-center opacity-20"
+          />
+          <Navbar />
+          <div className="z-20 mx-auto flex h-full w-full max-w-screen-2xl flex-grow">{children}</div>
+          <Footer />
+        </body>
+      </html>
+    //</SessionWrapper>
   );
 }
