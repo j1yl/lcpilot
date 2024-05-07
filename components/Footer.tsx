8c7f0003d8e8a115b936d4abccd730d3064287a4
Joe Lee <lefodev@gmail.com>
2024-05-07 12:04:20 -0700

    Change text size

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="flex h-[40px] w-full items-center justify-between bg-white/10 px-4 text-xs backdrop-blur-md">
-      <p className="text-sm">© 2024 Joe Lee & . All rights reserved.</p>
-      <Link href="https://opensource.org/license/mit" target="_blank" className="text-sm">
+      <p>© 2024 Joe Lee & . All rights reserved.</p>
+      <Link href="https://opensource.org/license/mit" target="_blank">
         MIT License
       </Link>
     </footer>
