481f03bd1b6555153dd349d67621400e44cf4d71
Joe Lee <lefodev@gmail.com>
2024-05-08 14:41:47 -0700

    Remove cp

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,8 +4,17 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <footer className="flex h-[40px] w-full items-center justify-between bg-white/10 px-4 text-xs backdrop-blur-md">
-      <p>© 2024 Joe Lee & . All rights reserved.</p>
-      <Link href="https://opensource.org/license/mit" target="_blank">
+      <div>
+        Built with{' '}
+        <Link href="https://nextjs.org/" target="_blank" className="underline hover:no-underline">
+          Next.js
+        </Link>
+      </div>
+      <Link
+        href="https://opensource.org/license/mit"
+        target="_blank"
+        className="underline hover:no-underline"
+      >
         MIT License
       </Link>
     </footer>
