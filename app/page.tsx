a2f171f831e76abbf2a13bc8e0a349e631ce61bc
Joe Lee <lefodev@gmail.com>
2024-05-08 10:50:17 -0700

    Add marquee effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Banner from '@/components/Banner';
 import getDocuments from '@/lib/firebase/getDocs';
 import { Problem } from '@/types/problem';
 import Link from 'next/link';
@@ -16,27 +17,30 @@ export default async function Home() {
   }
 
   return (
-    <div className="mx-auto flex w-full max-w-2xl flex-grow">
-      <ul className="mx-auto my-auto flex w-full flex-col gap-2">
-        {result
-          .sort((a, b) => a.lc_number - b.lc_number)
-          .map((doc) => (
-            <Link
-              href={`/problems/${doc.id}`}
-              key={doc.id}
-              className="group flex items-center justify-between gap-2"
-            >
-              <h2 className="underline group-hover:no-underline">
-                {doc.title[0].toUpperCase() + doc.title.slice(1).toLowerCase()}
-              </h2>
-              <span
-                className={`${doc.difficulty === 'easy' ? 'text-green-500' : doc.difficulty === 'medium' ? 'text-yellow-500' : 'text-red-500'}`}
+    <>
+      <div className="z-20 mx-auto flex w-full max-w-2xl flex-grow">
+        <ul className="mx-auto my-auto flex w-full flex-col gap-2">
+          {result
+            .sort((a, b) => a.lc_number - b.lc_number)
+            .map((doc) => (
+              <Link
+                href={`/problems/${doc.id}`}
+                key={doc.id}
+                className="group flex items-center justify-between gap-2"
               >
-                {doc.difficulty[0].toUpperCase() + doc.difficulty.slice(1).toLowerCase()}
-              </span>
-            </Link>
-          ))}
-      </ul>
-    </div>
+                <h2 className="underline group-hover:no-underline">
+                  {doc.title[0].toUpperCase() + doc.title.slice(1).toLowerCase()}
+                </h2>
+                <span
+                  className={`${doc.difficulty === 'easy' ? 'text-green-500' : doc.difficulty === 'medium' ? 'text-yellow-500' : 'text-red-500'}`}
+                >
+                  {doc.difficulty[0].toUpperCase() + doc.difficulty.slice(1).toLowerCase()}
+                </span>
+              </Link>
+            ))}
+        </ul>
+      </div>
+      <Banner />
+    </>
   );
 }
