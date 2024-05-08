bed2b750b2f49aa97cab611e61437d1b3d47a20f
Joe Lee <lefodev@gmail.com>
2024-05-08 10:29:48 -0700

    Style home

--- app/page.tsx
+++ app/page.tsx
@@ -16,29 +16,27 @@ export default async function Home() {
   }
 
   return (
-    <div className="mx-auto flex w-full max-w-6xl flex-grow">
-      <div className="mx-auto flex w-full flex-col p-4">
-        <table className="w-full">
-          <tr className="text-left">
-            <th>Title</th>
-            <th>Difficulty</th>
-          </tr>
-          {result.map((doc) => (
-            <tr key={doc.id}>
-              <td>
-                <Link href={`/problems/${doc.id}`}>
-                  {doc.title[0].toUpperCase() + doc.title.slice(1).toLowerCase()}
-                </Link>
-              </td>
-              <td
+    <div className="mx-auto flex w-full max-w-2xl flex-grow">
+      <ul className="mx-auto my-auto flex w-full flex-col gap-2">
+        {result
+          .sort((a, b) => a.lc_number - b.lc_number)
+          .map((doc) => (
+            <Link
+              href={`/problems/${doc.id}`}
+              key={doc.id}
+              className="group flex items-center justify-between gap-2"
+            >
+              <h2 className="underline group-hover:no-underline">
+                {doc.title[0].toUpperCase() + doc.title.slice(1).toLowerCase()}
+              </h2>
+              <span
                 className={`${doc.difficulty === 'easy' ? 'text-green-500' : doc.difficulty === 'medium' ? 'text-yellow-500' : 'text-red-500'}`}
               >
                 {doc.difficulty[0].toUpperCase() + doc.difficulty.slice(1).toLowerCase()}
-              </td>
-            </tr>
+              </span>
+            </Link>
           ))}
-        </table>
-      </div>
+      </ul>
     </div>
   );
 }
